2ecb93c9c64a0f5bb0f069cc082847f229d08975
Immax007 <101760591+Immax007@users.noreply.github.com>
2026-09-08 04:46:38 +0100

    Add Navbar component with menu and buttons

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import "./Navbar.css";
+
+
+function Navbar(){
+
+const [open,setOpen] = useState(false);
+
+
+return (
+
+<nav className="new-navbar">
+
+
+<div className="new-logo">
+🍃 The Olive Garden
+</div>
+
+
+<button 
+className="menu-button"
+onClick={()=>setOpen(!open)}
+>
+☰
+</button>
+
+
+
+<div className={`new-links ${open ? "show-menu":""}`}>
+
+<a href="#">Home</a>
+<a href="#">Menu</a>
+<a href="#">About</a>
+<a href="#">Contact</a>
+
+<button className="login-btn">
+Login
+</button>
+
+<button className="signup-btn">
+Sign Up
+</button>
+
+</div>
+
+
+</nav>
+
+)
+
+}
+
+
+export default Navbar;
