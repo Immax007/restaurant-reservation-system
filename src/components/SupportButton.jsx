8fa42dac3ee3c0703642afa99831f00fd3c24832
Immax007 <101760591+Immax007@users.noreply.github.com>
2026-09-08 04:13:58 +0100

    Add SupportButton component for customer support

---
+++ src/components/SupportButton.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./SupportButton.css";
+
+function SupportButton(){
+
+return (
+
+<button className="support-button">
+
+💬
+
+<span>
+Customer Support
+</span>
+
+</button>
+
+);
+
+}
+
+
+export default SupportButton;
